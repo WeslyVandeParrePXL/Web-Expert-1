f4a723dc1b094654c3e55d7ef521181eda83c4dd
WeslyVandeParrePXL <wesly.vandeparre@student.pxl.be>
2022-09-21 10:07:16 +0200

    commit 21/09

--- opdracht 2.js
+++ opdracht 2.js
@@ -1,12 +1,13 @@
-let inkoopprijs = parseInt(prompt("geef een inkoopprijs in"));
-let btw = parseInt(prompt("Geef een BTW-tarief in"));
+let inkoopprijs = parseFloat(prompt("geef een inkoopprijs in"));
+let btw = parseFloat(prompt("Geef een BTW-tarief in"));
 
 let verkoopprijs = inkoopprijs * (btw / 100) + inkoopprijs;
 
 alert("de verkoopprijs verkoopprijs = " + verkoopprijs + "€");
 
-/*if (inkoopprijs || btw === null) {
-    alert("Je hebt geen getal ingegeven")
+if (inkoopprijs && btw) {
+    alert("de verkoopprijs = " + (verkoopprijs + (verkoopprijs * btw / 100)));
 }
-*/
-
+else {
+    alert("oeps er ging iets fout");
+}
--- opdracht 3.js
+++ opdracht 3.js
@@ -2,6 +2,14 @@ let dagen = ["maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag","zo
 dagen.forEach((dag) => console.log(dag));
 
 alert("de lengte van de array is " + dagen.length);
+//alert(' de lengte van de array: ${dagen.length}')
+//interpolation
 
-dagen.splice(5,1);
+for (let i = 0; i < dagen.length; i++) {
+    if (dagen[i] === "zaterdag") {
+    dagen.splice(i, 1);
+    }
+}
+
+//dagen.splice(5,1);
 console.log(dagen);
